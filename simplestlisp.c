014c7e7084aad7809ac4d3cf11dbffb499b1b17f
kevindotklein <kevindotklein@gmail.com>
2023-01-20 17:05:44 -0300

    help and quit

--- simplestlisp.c
+++ simplestlisp.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-#define EXPR_LIMIT 256
+#define EXPR_LIMIT 4096
 #define SL_ADD '+'
 #define SL_SUB '-'
 #define SL_MUL '*'
 #define SL_DIV '/'
+#define SL_QUIT 'q'
+#define SL_HELP 'h'
 
 char *simplestlisp_read();
 int simplestlisp_eval(char *expr);
@@ -13,6 +16,7 @@ float simplestlisp_add(char *expr);
 float simplestlisp_mult(char *expr);
 float simplestlisp_div(char *expr);
 float simplestlisp_sub(char *expr);
+void simplestlisp_print_help();
 
 int main(int argc, char const **argv){
     (void) argc;
@@ -42,6 +46,12 @@ float simplestlisp_add(char *expr){
         float a = 0;
         sscanf(expr++, "%f", &a);
         sum += a;
+
+        while(*expr != ' '){
+            if(*expr == ')') break;
+            expr++;
+            continue;
+        }
     }
 
     return sum;
@@ -60,6 +70,11 @@ float simplestlisp_mult(char *expr){
         sscanf(expr++, "%f", &a);
         product *= a;
         
+        while(*expr != ' '){
+            if(*expr == ')') break;
+            expr++;
+            continue;
+        }
     }
 
     return product;
@@ -86,8 +101,17 @@ float simplestlisp_sub(char *expr){
             continue;
         }
 
+        while(*expr != ' '){
+            if(*expr == ')'){
+                return subtraction;
+            }
+            expr++;
+            continue;
+        }
+
         sscanf(expr++, "%f", &a);
         subtraction -= a;
+
     }
 
     return subtraction;
@@ -109,49 +133,82 @@ float simplestlisp_div(char *expr){
     }
 
     while(*expr != ')'){
-        if(*expr == 0){
-            fprintf(stdout, "div by 0 :(\n");
-            exit(0);
-        }
         if(*expr == ' '){
+            expr++;
+            continue;
+        }
+
+        while(*expr != ' '){
+            if(*expr == ')'){
+                return division;
+            }
             expr++;
             continue;
         }
 
         sscanf(expr++, "%f", &a);
         division /= a;
+
     }
 
     return division;
 }
 
+void simplestlisp_print_help(){
+    fprintf(stdout, "the simplestlisp is a simple interpreter\n");
+    fprintf(stdout, "type `:q` to quit\n");
+    fprintf(stdout, "u can evaluate few math expressions, such as + - * /\n");
+    fprintf(stdout, "try to follow this template for the operations above `(<operand> <number_1> <number_2> <number_n>)`\n");
+}
+
 int simplestlisp_eval(char *expr){
-    if(*(expr++) != '('){
-        fprintf(stderr, "must begin with parentheses\n");
-        return 1;
-    }
-    char *op = expr++;
 
     float result = 0;
 
-    switch (*op){
-    case SL_ADD:
-        result = simplestlisp_add(expr);
-        break;
-    case SL_MUL:
-        result = simplestlisp_mult(expr);
-        break;
-    case SL_DIV:
-        result = simplestlisp_div(expr);
-        break;
-    case SL_SUB:
-        result = simplestlisp_sub(expr);
-        break;
-    
-    default:
-        result = 0;
-        break;
-    }
+    if(*expr == ':'){
+        expr++;
+        char *op = expr++;
 
+        switch (*op)
+        {
+        case SL_QUIT:
+            exit(0);
+            break;
+        case SL_HELP:
+            simplestlisp_print_help();
+            return 0;
+            break;
+
+        default:
+            break;
+        }
+    }else{
+        if(*(expr++) != '('){
+            fprintf(stderr, "try `:h` for help\n");
+            return 0;
+        }
+
+        char *op = expr++;
+
+
+        switch (*op){
+        case SL_ADD:
+            result = simplestlisp_add(expr);
+            break;
+        case SL_MUL:
+            result = simplestlisp_mult(expr);
+            break;
+        case SL_DIV:
+            result = simplestlisp_div(expr);
+            break;
+        case SL_SUB:
+            result = simplestlisp_sub(expr);
+            break;
+        
+        default:
+            result = 0;
+            break;
+        }
+    }
     return result;
 }
